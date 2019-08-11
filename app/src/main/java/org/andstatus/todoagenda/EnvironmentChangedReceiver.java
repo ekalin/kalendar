3a6bbf35cace37c235fbaef36f938e90eccb15ac
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2019-08-11 08:48:39 -0300

    Refactors EnvironmentChangedReceiver.
    Cherry-picked from yvolk's 73c906b.

--- app/src/main/java/org/andstatus/todoagenda/EnvironmentChangedReceiver.java
+++ app/src/main/java/org/andstatus/todoagenda/EnvironmentChangedReceiver.java
@@ -6,17 +6,22 @@ import android.content.Intent;
 import android.util.Log;
 
 public class EnvironmentChangedReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(this.getClass().getName(), "Received intent: " + intent);
-        String action = intent.getAction();
-        if (action.equals(Intent.ACTION_LOCALE_CHANGED)
-                || action.equals(Intent.ACTION_TIME_CHANGED)
-                || action.equals(Intent.ACTION_DATE_CHANGED)
-                || action.equals(Intent.ACTION_TIMEZONE_CHANGED)) {
-            EventAppWidgetProvider.updateAllWidgets(context);
+        String action = intent == null
+                ? ""
+                : (intent.getAction() == null ? "" : intent.getAction());
+        switch (action) {
+            case Intent.ACTION_LOCALE_CHANGED:
+            case Intent.ACTION_TIME_CHANGED:
+            case Intent.ACTION_DATE_CHANGED:
+            case Intent.ACTION_TIMEZONE_CHANGED:
+                EventAppWidgetProvider.updateAllWidgets(context);
+                break;
+            default:
+                EventAppWidgetProvider.updateEventList(context);
+                break;
         }
-        EventAppWidgetProvider.updateEventList(context);
     }
 }
