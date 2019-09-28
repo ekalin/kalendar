plusonelabs/calendar-widget

Fix widget updates (including data races)

app/src/main/java/org/andstatus/todoagenda/EnvironmentChangedReceiver.java
@@ -1,38 +1,72 @@
 package org.andstatus.todoagenda;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import org.andstatus.todoagenda.prefs.AllSettings;
+import org.andstatus.todoagenda.prefs.InstanceSettings;
 import org.andstatus.todoagenda.provider.EventProviderType;
+import org.andstatus.todoagenda.util.DateUtil;
+import org.joda.time.DateTime;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class EnvironmentChangedReceiver extends BroadcastReceiver {
     private static final AtomicReference<EnvironmentChangedReceiver> registeredReceiver = new AtomicReference<>();
 
-    public static void registerReceivers(Context contextIn) {
-        Context context = contextIn.getApplicationContext();
+    public static void registerReceivers(Map<Integer, InstanceSettings> instances) {
+        if (instances.isEmpty()) return;
+
+        InstanceSettings instanceSettings = instances.values().iterator().next();
+        Context context = instanceSettings.getContext().getApplicationContext();
         synchronized (registeredReceiver) {
             EnvironmentChangedReceiver receiver = new EnvironmentChangedReceiver();
             EventProviderType.registerProviderChangedReceivers(context, receiver);
 
-            IntentFilter userPresent = new IntentFilter();
-            userPresent.addAction("android.intent.action.USER_PRESENT");
-            context.registerReceiver(receiver, userPresent);
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_USER_PRESENT);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                filter.addAction(Intent.ACTION_DREAMING_STOPPED);
+            }
+            context.registerReceiver(receiver, filter);
 
             EnvironmentChangedReceiver oldReceiver = registeredReceiver.getAndSet(receiver);
             if (oldReceiver != null) {
                 oldReceiver.unRegister(context);
             }
+            scheduleNextAlarms(context, instances);
 
-            Log.i(EventAppWidgetProvider.class.getName(),
-                    "Registered receivers from " + contextIn.getClass().getName());
+            Log.i(AppWidgetProvider.class.getName(),
+                    "Registered receivers from " + instanceSettings.getContext().getClass().getName());
+        }
+    }
+
+    private static void scheduleNextAlarms(Context context, Map<Integer, InstanceSettings> instances) {
+        Set<DateTime> alarmTimes = new HashSet<>();
+        for (InstanceSettings settings : instances.values()) {
+            alarmTimes.add(DateUtil.now(settings.getTimeZone()).withTimeAtStartOfDay().plusDays(1));
+        }
+        int counter = 0;
+        for (DateTime alarmTime : alarmTimes) {
+            Intent intent = new Intent(context, EnvironmentChangedReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    AppWidgetProvider.REQUEST_CODE_MIDNIGHT_ALARM + counter,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            am.set(AlarmManager.RTC, alarmTime.getMillis(), pendingIntent);
+            counter++;
         }
     }
 
@@ -48,28 +82,22 @@ public class EnvironmentChangedReceiver extends BroadcastReceiver {
                 ? ""
                 : (intent.getAction() == null ? "" : intent.getAction());
         switch (action) {
-            case Intent.ACTION_LOCALE_CHANGED:
-            case Intent.ACTION_TIME_CHANGED:
-            case Intent.ACTION_DATE_CHANGED:
-            case Intent.ACTION_TIMEZONE_CHANGED:
-                EventAppWidgetProvider.updateWidgetsWithData(context);
-                break;
-            case EventAppWidgetProvider.ACTION_GOTO_POSITIONS:
+            case AppWidgetProvider.ACTION_GOTO_POSITIONS:
                 int widgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 0);
-                int position1 = intent.getIntExtra(EventAppWidgetProvider.EXTRA_WIDGET_LIST_POSITION1, 0);
-                int position2 = intent.getIntExtra(EventAppWidgetProvider.EXTRA_WIDGET_LIST_POSITION2, 0);
+                int position1 = intent.getIntExtra(AppWidgetProvider.EXTRA_WIDGET_LIST_POSITION1, 0);
+                int position2 = intent.getIntExtra(AppWidgetProvider.EXTRA_WIDGET_LIST_POSITION2, 0);
                 gotoPosition(context, widgetId, position1);
                 if (position1 >= 0 && position2 >= 0 && position1 != position2) {
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        // Ignored
                     }
                 }
                 gotoPosition(context, widgetId, position2);
                 break;
             default:
-                EventAppWidgetProvider.updateEventList(context);
+                AppWidgetProvider.updateAllWidgets(context);
                 break;
         }
     }

app/src/main/java/org/andstatus/todoagenda/WidgetConfigurationActivity.java
@@ -32,7 +32,7 @@ public class WidgetConfigurationActivity extends PreferenceActivity {
     protected void onPause() {
         super.onPause();
         ApplicationPreferences.save(this, widgetId);
-        EventAppWidgetProvider.updateWidgetWithData(this, widgetId);
+        AppWidgetProvider.updateWidget(this, widgetId);
     }
 
     @Override

app/src/main/java/org/andstatus/todoagenda/prefs/AllSettings.java
@@ -3,7 +3,8 @@ package org.andstatus.todoagenda.prefs;
 import android.content.Context;
 import android.util.Log;
 
-import org.andstatus.todoagenda.EventAppWidgetProvider;
+import org.andstatus.todoagenda.AppWidgetProvider;
+import org.andstatus.todoagenda.EnvironmentChangedReceiver;
 import org.andstatus.todoagenda.R;
 import org.andstatus.todoagenda.provider.EventProviderType;
 import org.json.JSONArray;
@@ -15,7 +16,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import androidx.annotation.NonNull;
 
-import static org.andstatus.todoagenda.EventAppWidgetProvider.getWidgetIds;
+import static org.andstatus.todoagenda.AppWidgetProvider.getWidgetIds;
 import static org.andstatus.todoagenda.prefs.SettingsStorage.loadJsonFromFile;
 
 /**
@@ -48,7 +49,8 @@ public class AllSettings {
                     settings.logMe(AllSettings.class, "newInstance put", widgetId);
                     instances.put(widgetId, settings);
                     EventProviderType.initialize(context, true);
-                    EventAppWidgetProvider.updateWidgetWithData(context, widgetId);
+                    EnvironmentChangedReceiver.registerReceivers(instances);
+                    AppWidgetProvider.updateWidget(context, widgetId);
                 }
             }
             return settings;
@@ -78,6 +80,7 @@ public class AllSettings {
                     }
                 }
                 instancesLoaded = true;
+                EnvironmentChangedReceiver.registerReceivers(instances);
             }
         }
     }
@@ -99,6 +102,7 @@ public class AllSettings {
                 }
             }
             instancesLoaded = true;
+            EnvironmentChangedReceiver.registerReceivers(instances);
         }
     }
 

ekalin/kalendar

app/src/main/java/org/andstatus/todoagenda/EnvironmentChangedReceiver.java
@@ -1,21 +1,36 @@
 package org.andstatus.todoagenda;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.util.Log;
 
 import org.andstatus.todoagenda.prefs.AllSettings;
+import org.andstatus.todoagenda.prefs.InstanceSettings;
 import org.andstatus.todoagenda.task.dmfs.DmfsOpenTasksContract;
+import org.andstatus.todoagenda.util.DateUtil;
+import org.joda.time.DateTime;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class EnvironmentChangedReceiver extends BroadcastReceiver {
     private static final AtomicReference<EnvironmentChangedReceiver> registeredReceiver = new AtomicReference<>();
 
-    public static void registerReceivers(Context contextIn) {
-        Context context = contextIn.getApplicationContext();
+    public static void registerReceivers(Map<Integer, InstanceSettings> instances) {
+        if (instances.isEmpty()) {
+            return;
+        }
+
+        InstanceSettings instanceSettings = instances.values().iterator().next();
+        Context context = instanceSettings.getContext().getApplicationContext();
         synchronized (registeredReceiver) {
             EnvironmentChangedReceiver receiver = new EnvironmentChangedReceiver();
 
@@ -26,17 +41,39 @@ public class EnvironmentChangedReceiver extends BroadcastReceiver {
             providerChanged.addDataAuthority(DmfsOpenTasksContract.AUTHORITY, null);
             context.registerReceiver(receiver, providerChanged);
 
-            IntentFilter userPresent = new IntentFilter();
-            userPresent.addAction("android.intent.action.USER_PRESENT");
-            context.registerReceiver(receiver, userPresent);
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_USER_PRESENT);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                filter.addAction(Intent.ACTION_DREAMING_STOPPED);
+            }
+            context.registerReceiver(receiver, filter);
 
             EnvironmentChangedReceiver oldReceiver = registeredReceiver.getAndSet(receiver);
             if (oldReceiver != null) {
                 oldReceiver.unRegister(context);
             }
+            scheduleNextAlarms(context, instances);
 
             Log.i(EventAppWidgetProvider.class.getName(),
-                    "Registered receivers from " + contextIn.getClass().getName());
+                    "Registered receivers from " + instanceSettings.getContext().getClass().getName());
+        }
+    }
+
+    private static void scheduleNextAlarms(Context context, Map<Integer, InstanceSettings> instances) {
+        Set<DateTime> alarmTimes = new HashSet<>();
+        for (InstanceSettings settings : instances.values()) {
+            alarmTimes.add(DateUtil.now(settings.getTimeZone()).withTimeAtStartOfDay().plusDays(1));
+        }
+        int counter = 0;
+        for (DateTime alarmTime : alarmTimes) {
+            Intent intent = new Intent(context, EnvironmentChangedReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    EventAppWidgetProvider.REQUEST_CODE_MIDNIGHT_ALARM + counter,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            am.set(AlarmManager.RTC, alarmTime.getMillis(), pendingIntent);
+            counter++;
         }
     }
 
@@ -44,23 +81,11 @@ public class EnvironmentChangedReceiver extends BroadcastReceiver {
         context.unregisterReceiver(this);
     }
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(this.getClass().getName(), "Received intent: " + intent);
         AllSettings.ensureLoadedFromFiles(context, false);
-        String action = intent == null
-                ? ""
-                : (intent.getAction() == null ? "" : intent.getAction());
-        switch (action) {
-            case Intent.ACTION_LOCALE_CHANGED:
-            case Intent.ACTION_TIME_CHANGED:
-            case Intent.ACTION_DATE_CHANGED:
-            case Intent.ACTION_TIMEZONE_CHANGED:
-                EventAppWidgetProvider.updateWidgetsWithData(context);
-                break;
-            default:
-                EventAppWidgetProvider.updateEventList(context);
-                break;
-        }
+        EventAppWidgetProvider.updateAllWidgets(context);
     }
 }

app/src/main/java/org/andstatus/todoagenda/WidgetConfigurationActivity.java
@@ -100,7 +100,7 @@ public class WidgetConfigurationActivity extends AppCompatActivity
     protected void onPause() {
         super.onPause();
         ApplicationPreferences.save(this, widgetId);
-        EventAppWidgetProvider.updateWidgetWithData(this, widgetId);
+        EventAppWidgetProvider.updateWidget(this, widgetId);
     }
 
     @Override

app/src/main/java/org/andstatus/todoagenda/prefs/AllSettings.java
@@ -2,7 +2,10 @@ package org.andstatus.todoagenda.prefs;
 
 import android.content.Context;
 import android.util.Log;
+import androidx.annotation.NonNull;
 
+import org.andstatus.todoagenda.EnvironmentChangedReceiver;
+import org.andstatus.todoagenda.EventAppWidgetProvider;
 import org.andstatus.todoagenda.R;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -10,8 +13,6 @@ import org.json.JSONObject;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import androidx.annotation.NonNull;
 
 import static org.andstatus.todoagenda.EventAppWidgetProvider.getWidgetIds;
 import static org.andstatus.todoagenda.prefs.SettingsStorage.loadJsonFromFile;
@@ -45,6 +46,8 @@ public class AllSettings {
                     settings.save();
                     settings.logMe(AllSettings.class, "newInstance put", widgetId);
                     instances.put(widgetId, settings);
+                    EnvironmentChangedReceiver.registerReceivers(instances);
+                    EventAppWidgetProvider.updateWidget(context, widgetId);
                 }
             }
             return settings;
@@ -73,6 +76,7 @@ public class AllSettings {
                     }
                 }
                 instancesLoaded = true;
+                EnvironmentChangedReceiver.registerReceivers(instances);
             }
         }
     }
@@ -93,6 +97,7 @@ public class AllSettings {
                 }
             }
             instancesLoaded = true;
+            EnvironmentChangedReceiver.registerReceivers(instances);
         }
     }
 