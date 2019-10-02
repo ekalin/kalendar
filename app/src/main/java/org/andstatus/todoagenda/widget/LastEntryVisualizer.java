plusonelabs/calendar-widget

Refactoring: move methods closer to their usage

app/src/main/java/org/andstatus/todoagenda/widget/LastEntryVisualizer.java
@@ -14,7 +14,7 @@ import org.joda.time.DateTime;
 import java.util.Collections;
 import java.util.List;
 
-import static org.andstatus.todoagenda.AppWidgetProvider.getPermittedAddEventPendingIntent;
+import static org.andstatus.todoagenda.RemoteViewsFactory.getPermittedAddEventPendingIntent;
 import static org.andstatus.todoagenda.util.CalendarIntentUtil.createOpenCalendarAtDayIntent;
 import static org.andstatus.todoagenda.util.RemoteViewsUtil.setBackgroundColor;
 import static org.andstatus.todoagenda.util.RemoteViewsUtil.setTextColorFromAttr;

ekalin/kalendar

app/src/main/java/org/andstatus/todoagenda/widget/LastEntryVisualizer.java
@@ -13,7 +13,7 @@ import org.joda.time.DateTime;
 import java.util.Collections;
 import java.util.List;
 
-import static org.andstatus.todoagenda.EventAppWidgetProvider.getPermittedAddEventPendingIntent;
+import static org.andstatus.todoagenda.EventRemoteViewsFactory.getPermittedAddEventPendingIntent;
 import static org.andstatus.todoagenda.util.CalendarIntentUtil.createOpenCalendarAtDayIntent;
 import static org.andstatus.todoagenda.util.RemoteViewsUtil.setTextColorFromAttr;
 import static org.andstatus.todoagenda.util.RemoteViewsUtil.setTextSize;