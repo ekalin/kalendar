238208cdecdddc4d90807f9e39a4f3e5b40266fc
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2019-11-03 09:29:13 -0300

    Simplify handling of widget name changes.
    Also prevents an empty name from being set.

--- app/src/main/java/org/andstatus/todoagenda/prefs/LayoutPreferencesFragment.java
+++ app/src/main/java/org/andstatus/todoagenda/prefs/LayoutPreferencesFragment.java
@@ -1,76 +1,36 @@
 package org.andstatus.todoagenda.prefs;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 
-import org.andstatus.todoagenda.MainActivity;
 import org.andstatus.todoagenda.R;
 import org.andstatus.todoagenda.util.DateUtil;
 import org.joda.time.DateTimeZone;
 
 import java.util.TimeZone;
 
-public class LayoutPreferencesFragment extends KalendarPreferenceFragment
-        implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class LayoutPreferencesFragment extends KalendarPreferenceFragment {
     private static final String PREF_LOCK_TIME_ZONE = "lockTimeZone";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
         setPreferencesFromResource(R.xml.preferences_layout, rootKey);
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
+        Preference prefWidgetInstanceName = findPreference(InstanceSettings.PREF_WIDGET_INSTANCE_NAME);
+        prefWidgetInstanceName.setOnPreferenceChangeListener(this::checkName);
+        prefWidgetInstanceName.setSummaryProvider(preference -> getNameAndId());
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        showWidgetInstanceName();
         showLockTimeZone(true);
-        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        switch (key) {
-            case InstanceSettings.PREF_WIDGET_INSTANCE_NAME:
-                getActivity().finish();
-                startActivity(MainActivity.intentToConfigure(getActivity(), instanceSettings
-                        .getWidgetId()));
-                break;
-            default:
-                break;
-        }
+    private boolean checkName(Preference preference, Object newValue) {
+        return !((String) newValue).isEmpty();
     }
 
-    private void showWidgetInstanceName() {
-        Preference preference = findPreference(InstanceSettings.PREF_WIDGET_INSTANCE_NAME);
-        if (preference != null) {
-            preference.setSummary(instanceSettings.getWidgetInstanceName() +
-                    " (id:" + instanceSettings.getWidgetId() + ")");
-        }
-    }
-
-    private void showLockTimeZone(boolean setAlso) {
-        CheckBoxPreference preference = findPreference(PREF_LOCK_TIME_ZONE);
-        if (preference != null) {
-            boolean isChecked = setAlso ? instanceSettings.isTimeZoneLocked() : preference.isChecked();
-            if (setAlso && preference.isChecked() != isChecked) {
-                preference.setChecked(isChecked);
-            }
-            DateTimeZone timeZone = DateTimeZone.forID(DateUtil.validatedTimeZoneId(isChecked ?
-                    instanceSettings.getLockedTimeZoneId() : TimeZone.getDefault().getID()));
-            preference.setSummary(String.format(
-                    getText(isChecked ? R.string.lock_time_zone_on_desc : R.string.lock_time_zone_off_desc).toString(),
-                    timeZone.getName(DateUtil.now(timeZone).getMillis()))
-            );
-        }
+    private String getNameAndId() {
+        return instanceSettings.getWidgetInstanceName() + " (id:" + instanceSettings.getWidgetId() + ")";
     }
 
     @Override
@@ -87,5 +47,21 @@ public class LayoutPreferencesFragment extends KalendarPreferenceFragment
                 break;
         }
         return super.onPreferenceTreeClick(preference);
+    }
+
+    private void showLockTimeZone(boolean setAlso) {
+        CheckBoxPreference preference = findPreference(PREF_LOCK_TIME_ZONE);
+        if (preference != null) {
+            boolean isChecked = setAlso ? instanceSettings.isTimeZoneLocked() : preference.isChecked();
+            if (setAlso && preference.isChecked() != isChecked) {
+                preference.setChecked(isChecked);
+            }
+            DateTimeZone timeZone = DateTimeZone.forID(DateUtil.validatedTimeZoneId(isChecked ?
+                    instanceSettings.getLockedTimeZoneId() : TimeZone.getDefault().getID()));
+            preference.setSummary(String.format(
+                    getText(isChecked ? R.string.lock_time_zone_on_desc : R.string.lock_time_zone_off_desc).toString(),
+                    timeZone.getName(DateUtil.now(timeZone).getMillis()))
+            );
+        }
     }
 }
