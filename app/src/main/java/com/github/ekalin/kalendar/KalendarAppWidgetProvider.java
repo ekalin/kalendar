baa9d0c52bd130b79f8d2b4a960c5dbb2a37985a
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2024-01-24 14:12:07 -0300

    Remove unnecessary override

--- app/src/main/java/com/github/ekalin/kalendar/KalendarAppWidgetProvider.java
+++ app/src/main/java/com/github/ekalin/kalendar/KalendarAppWidgetProvider.java
@@ -4,8 +4,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.github.ekalin.kalendar.prefs.AllSettings;
@@ -14,21 +12,19 @@ public class KalendarAppWidgetProvider extends AppWidgetProvider {
     private static final String TAG = KalendarAppWidgetProvider.class.getSimpleName();
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(action)) {
-            Bundle extras = intent.getExtras();
-            int[] widgetIds = extras == null
-                    ? null
-                    : extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-            if (widgetIds == null || widgetIds.length == 0) {
-                widgetIds = getWidgetIds(context);
-            }
-            if (widgetIds != null && widgetIds.length > 0) {
-                onUpdate(context, AppWidgetManager.getInstance(context), widgetIds);
-            }
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            KalendarRemoteViewsFactory.updateWidget(context, widgetId, null);
+            notifyWidgetDataChanged(context, widgetId);
+        }
+    }
+
+    private static void notifyWidgetDataChanged(Context context, int widgetId) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        if (appWidgetManager != null) {
+            appWidgetManager.notifyAppWidgetViewDataChanged(new int[]{widgetId}, R.id.event_list);
         } else {
-            super.onReceive(context, intent);
+            Log.d(TAG, widgetId + " notifyWidgetDataChanged, appWidgetManager is null, context:" + context);
         }
     }
 
@@ -40,27 +36,10 @@ public class KalendarAppWidgetProvider extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int widgetId : appWidgetIds) {
-            KalendarRemoteViewsFactory.updateWidget(context, widgetId, null);
-            notifyWidgetDataChanged(context, widgetId);
-        }
-    }
-
     public static int[] getWidgetIds(Context context) {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         return appWidgetManager == null
                 ? new int[]{}
                 : appWidgetManager.getAppWidgetIds(new ComponentName(context, KalendarAppWidgetProvider.class));
-    }
-
-    private static void notifyWidgetDataChanged(Context context, int widgetId) {
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        if (appWidgetManager != null) {
-            appWidgetManager.notifyAppWidgetViewDataChanged(new int[]{widgetId}, R.id.event_list);
-        } else {
-            Log.d(TAG, widgetId + " notifyWidgetDataChanged, appWidgetManager is null, context:" + context);
-        }
     }
 }
